c4d1b384fc4df20afde03b862b8902b2b9ee7ec9
Joseph Handschu <jhandschu@gmail.com>
2015-12-11 13:16:42 -0500

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,25 @@
+import tools.Tools;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+<<<<<<< HEAD
+		System.out.println("Enter your name:");
+		// working on this new path
+		// need to implemnt the input....
+		//
+		
+		System.out.println("Hellow Horld !!");
+=======
+		Tools mc = new Tools();
+		
+		mc.DisplayHello();
+		
+		mc.DisplayBye();
+>>>>>>> test1
+		
+	}
+
+}
---
+++ tools/Tools.java
@@ -0,0 +1,15 @@
+package tools;
+
+public class Tools {
+	
+	public void DisplayHello()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public void DisplayBye()
+	{
+		System.out.println("Bye Workd");
+	}
+
+}
