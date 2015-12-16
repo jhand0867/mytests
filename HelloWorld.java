f9ac4f3424166ff373c698fd4418ef2f79703e03
Joseph Handschu <jhandschu@gmail.com>
2015-12-16 17:50:08 -0500

    deleting past erroneous commits.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,20 +5,18 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-<<<<<<< HEAD
 		System.out.println("Enter your name:");
 		// working on this new path
 		// need to implemnt the input....
 		//
 		
 		System.out.println("Hellow Horld !!");
-=======
+
 		Tools mc = new Tools();
 		
 		mc.DisplayHello();
 		
 		mc.DisplayBye();
->>>>>>> test1
 		
 	}
 
